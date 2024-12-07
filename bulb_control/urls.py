6d0635a4499698494822c53f060ec651310a3174
AdamMashaka <mashakaadam123@gmail.com>
2024-12-07 14:43:29 +0300

    yeah

--- bulb_control/urls.py
+++ bulb_control/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('', views.home, name='home'),  # Ensure this line is present
+    path('', views.home, name='home'),  
     path('sms/', views.sms_webhook, name='sms_webhook'),
     path('bulb-status/', views.bulb_status, name='bulb_status'),
     path('toggle-bulb/', views.toggle_bulb, name='toggle_bulb'),
