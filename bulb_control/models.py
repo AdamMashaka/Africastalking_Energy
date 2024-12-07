99ffb067582de23d5ad6a6ae79f85f1d0688ba21
AdamMashaka <mashakaadam123@gmail.com>
2024-12-07 14:43:04 +0300

    model

--- bulb_control/models.py
+++ bulb_control/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Bulb(models.Model):
-    state = models.BooleanField(default=False)  # False = Off, True = On
+    state = models.BooleanField(default=False)  
 
     def __str__(self):
         return "On" if self.state else "Off"
