2d22018bf5bf78184424ce44d58e8abe524d5a65
AdamMashaka <mashakaadam123@gmail.com>
2024-12-07 15:21:06 +0300

    waooh

--- bulb_control/views.py
+++ bulb_control/views.py
@@ -8,8 +8,8 @@ import json
 from django.shortcuts import render
 from .models import Bulb
 
-username = "your_username" 
-api_key = "your_api_key"   
+username = "sim_alert" 
+api_key = "atsk_6f8d5e4a837a68cfaccfa81922360f5e6d7e3082e0aca4a01314c2857ffe833ff25fda59"   
 africastalking.initialize(username, api_key)
 sms_service = africastalking.SMS
 
