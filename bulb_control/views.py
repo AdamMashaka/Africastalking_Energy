34a24b1e3c2582564e5b3fceb868692464e0e936
AdamMashaka <mashakaadam123@gmail.com>
2024-12-08 10:16:55 +0300

    view

--- bulb_control/views.py
+++ bulb_control/views.py
@@ -9,8 +9,9 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-username = "sandbox"
-api_key = "atsk_6f8d5e4a837a68cfaccfa81922360f5e6d7e3082e0aca4a01314c2857ffe833ff25fda59"  
+username = "AdamKatani"
+api_key = "your api key here"  
+
 africastalking.initialize(username, api_key)
 sms_service = africastalking.SMS
 
